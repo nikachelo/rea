f309a09701477a48019381cc00daab51391154c6
unknown <nikachelidze9@gmail.com>
2023-03-07 15:01:05 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>დალინკე განცხადება, ბევრი განცხადების შემთხვევაში გამოყავი ის მძიმით</p>  
+          <div>
+            <textarea rows="10" cols="50"></textarea>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
