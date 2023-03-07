dfda1615ee5ef7ff0f2a67e76e40934345311f84
unknown <nikachelidze9@gmail.com>
2023-03-07 17:46:02 +0400

    Prints in back with address and name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+function handleClick() {
+  const textBoxValue = document.querySelector('textarea').value;
+  fetch('https://limitedrevolvingbrowsers.nikachelidze.repl.co/receive_data', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      data: textBoxValue
+    })
+  })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>დალინკე განცხადება, ბევრი განცხადებsის შემთხვევაში გამოყავი ის მძიმით</p>  
+        <p>დალინკე განცხადება, ბევრი განცხადების შემთხვევაში გამოყავი ის მძიმით</p>  
           <div>
             <textarea rows="10" cols="50"></textarea>
           </div>
+          <button onClick={handleClick}>Click me!</button>
       </header>
     </div>
   );
